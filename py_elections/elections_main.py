f19a5885752e9f100c3a9c15b7f7872a861638fd
Terry Potter <tepotter@yahoo.com>
2020-02-22 14:53:34 -0600

    final solns

--- py_elections/elections_main.py
+++ py_elections/elections_main.py
@@ -8,17 +8,17 @@ import csv
 # Import module operator
 import operator
 
+# Path to CSV file
 csvpath = os.path.join('houston_election_data.csv')
 
-
-# Method 2: Improved Reading using CSV module
+# Reading using CSV module
 
 with open(csvpath, newline='', encoding="utf-8") as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    
+# Create dictionary using data from csvreader   
     cand_data = {}
     for row in csvreader:
         if row[0] not in cand_data.keys():
@@ -26,12 +26,17 @@ with open(csvpath, newline='', encoding="utf-8") as csvfile:
             
         else:
             cand_data[row[0]] += 1
-            
+
+# Delete header            
     cand_data.pop("Candidate")
+# Calculate total votes
     total_votes = (sum(cand_data.values())) 
+# Sort dictionary in descending order
     sorted_cand_data = dict( sorted(cand_data.items(), key=operator.itemgetter(1),reverse=True))
+# Create a list of dictionary keys (candidate names)
     adv_cand = list(sorted_cand_data.keys())
 
+# Write first part of output to a text file
     f = open("py_elections_results.txt", "w")
     f.write(
     f" Houston Mayoral Election Results \n"
@@ -39,13 +44,13 @@ with open(csvpath, newline='', encoding="utf-8") as csvfile:
     f"Total Cast Votes: {total_votes} \n"
     f"----------------------------------\n")
     f.close()
-    
+# Append previously created text file (candidate names and voting info)    
     for x, y in sorted_cand_data.items():
         z = round(((y/total_votes)*100),2)
         f = open("py_elections_results.txt", "a")
         f.write(f"{x}:  {z}%  ({y}) \n")
         f.close()
-
+# Final append to previously created text file
     f = open("py_elections_results.txt", "a")
     f.write(
     f"----------------------------------\n"
@@ -53,7 +58,7 @@ with open(csvpath, newline='', encoding="utf-8") as csvfile:
     f"2nd Advancing Candidate: {adv_cand[1]} \n"
     f"----------------------------------\n")  
     f.close()
-
+# Open text file and print to cmd window
     f = open("py_elections_results.txt", "r")
     print(f.read())
     f.close()
--- py_finances/finances_main.py
+++ py_finances/finances_main.py
@@ -5,54 +5,57 @@ from statistics import mean
 # Module for reading CSV files
 import csv
 
+# Path for csv file
 csvpath = os.path.join('budget_data.csv')
 
 
-# Method 2: Improved Reading using CSV module
+# Reading using CSV module
 
 with open(csvpath, newline='', encoding="utf-8") as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    #print(csvreader)
-
-    # Read the header row first (skip this step if there is no header)
-    #csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
-
-    # Read each row of data after the header
+    # Create list and read each row of data after the header
     fin_data = []
     for row in csvreader:
         fin_data.append(row[0:2])
         
-       
+    # Create a list for profits and loss data   
     prof_loss = []
+    # Create a list containing net month to month profit and loss
     site_limits = []
+    # Read profit and loss data from fin_data into prof_loss
     for row in fin_data:
         prof_loss.append(row[0])
-        #prof_loss = int(prof_loss)
+    # Delete header info
     prof_loss.pop(0)
-    #print(prof_loss)
-
+    # Read net month to month profit and loss data 
     for i in range(len(prof_loss)):
         if i != 0:
             site_limits.append(
                 int(prof_loss[i]) - int(prof_loss[i-1]))
-
+    # Find max profit/loss
     max_limit = max(site_limits)
+    # Find index of corresponding max profit/loss
     ind_max = site_limits.index(max_limit)
+    # Find min profit/loss
     min_limit = min(site_limits)
+    # Find index corresponding to min profit/loss
     ind_min = site_limits.index(min_limit)
+    # Find average profit/loss
     avg_site_limit = mean(site_limits)
+    # Find total profit/loss
     for i in range(0, len(prof_loss)):
         prof_loss[i] = int(prof_loss[i])
     sum_pl = sum(prof_loss)
 
-# +2 is correction from pop used above and loop
+# Create string containing MM YY corresponding to max and min profit/loss
+# +2 is correction from pop and loop used above
 x = (str(fin_data[ind_max +2])).split("'")
 y = (str(fin_data[ind_min +2])).split("'")
 
+# Create string containing output
 Results = (
 f" Financial Analysis \n"
 f"-------------------------\n"
@@ -61,10 +64,11 @@ f"Total: ${sum_pl} \n"
 f"Average Change: ${round(avg_site_limit,2)} \n"
 f"Greatest Increase in Profit: {x[3]} (${max_limit}) \n"
 f"Greatest Decrease in Profit: {y[3]} (${min_limit}) \n")
+# Print output to cmd screen
 print(Results)
-#create path to publish results
+# Create path to publish results and name file
 publish_data = os.path.join('py_finances_results.txt')
-#create the write file with results
+# Create the write file with results
 with open(publish_data, 'w') as text_file:
     text_writer = text_file.write(Results)
 
